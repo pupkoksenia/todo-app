a6fb998265c3c301dbeee9e3b5f75f54f77be271
Kseniya <pupkoksenia@gmail.com>
2022-09-30 14:36:36 +0300

    added firestore

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { createFirebaseConfig } from "../src/components/composables/createFireBaseConfig";
 
 createApp(App).use(router).mount("#app");
+
+const { firebaseConfig } = createFirebaseConfig();
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
