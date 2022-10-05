9fb609b8436df018707b215a6fc97412c03465d9
Kseniya <pupkoksenia@gmail.com>
2022-10-05 12:27:20 +0300

    style loader

--- src/components/staff/Loader.vue
+++ src/components/staff/Loader.vue
@@ -11,31 +11,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style>
-.loader-ring {
-  margin-left: 45%;
-  margin-top: 15%;
-  width: 100wv;
-  height: 100hv;
-}
-.loader-ring:after {
-  content: '';
-  display: block;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border-radius: 50%;
-  border: 6px solid #6366f1;
-  border-color: #6366f1 transparent #6366f1 transparent;
-  animation: loader-ring 1.2s linear infinite;
-}
-@keyframes loader-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
