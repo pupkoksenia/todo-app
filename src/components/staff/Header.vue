1688528a2723671ed5184f165b5d0288d4a9f27e
Kseniya <pupkoksenia@gmail.com>
2022-10-05 13:37:59 +0300

    remove dark mode

--- src/components/staff/Header.vue
+++ src/components/staff/Header.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="grid grid-cols-8 w-screen h-16">
     <div class="col-span-1 justify-items-end"></div>
-    <div class="flex items-center justify-start col-span-3 text-black font-bold dark:text-white">To-do app</div>
+    <div class="flex items-center justify-start col-span-3 text-black font-bold">To-do app</div>
 
-    <button class="flex items-center justify-center col-span-2 text-cyan-700 font-bold dark:text-violet-500">
-      Log Out
-    </button>
+    <button class="flex items-center justify-center col-span-2 text-cyan-700 font-bold">Log Out</button>
     <div class="col-span-1 flex items-center justify-items-center">
       <img src="../../assets/profile.jpg" class="h-8 w-8" />
     </div>
